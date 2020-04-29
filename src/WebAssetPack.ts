381fce4ebc4441fc4b5efdb0572a0667cd59b8dd
Norman Breau <norman@normanbreau.com>
2020-04-29 20:57:37 -0300

    Revert "(temp) removed pako compression"
    This reverts commit 68a3c34bd18d0caed7573597e4cdc9ee019d8b9a.

--- src/WebAssetPack.ts
+++ src/WebAssetPack.ts
@@ -1,5 +1,5 @@
 
-// import * as Pako from 'pako';
+import * as Pako from 'pako';
 import {
     Manifest,
     WAPPlugin,
@@ -26,8 +26,7 @@ export class WebAssetPack {
             throw new Error(`File "${path}" not found`);
         }
 
-        return new Uint8Array(this._buffer.slice(mfItem.start, mfItem.end));
-        // return Pako.inflateRaw(new Uint8Array(this._buffer.slice(mfItem.start, mfItem.end)))
+        return Pako.inflateRaw(new Uint8Array(this._buffer.slice(mfItem.start, mfItem.end)))
     }
 
     public async get<T = any>(path: string): Promise<T> {
